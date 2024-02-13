757412c59b8413a9a684a26930c10e8d4eb054a0
Grace Peters <petersograce@gmail.com>
2024-02-13 15:10:57 +0000

    feat: declare a base class called ‘Person’

---
+++ src/main/java/com/cbfacademy/apiassessment/person/Person.java
@@ -0,0 +1,17 @@
+package com.cbfacademy.apiassessment.person;
+
+import java.time.LocalDate;
+
+/**
+ * Represents a person with basic personal information. This class serves as a base
+ * to encapsulate common attributes that can be shared with other entities in the system.
+ * Attributes include first name, last name, date of birth, contact number, and email address.
+ */
+public class Person {
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private String contactNumber;
+    private String email;
+    
+}
