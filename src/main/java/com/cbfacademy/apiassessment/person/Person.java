23848686771f3d1e79fc1eadc20b4219a2950592
Grace Peters <petersograce@gmail.com>
2024-02-13 15:36:35 +0000

    feat: define a constructor method for the 'Person' class

--- src/main/java/com/cbfacademy/apiassessment/person/Person.java
+++ src/main/java/com/cbfacademy/apiassessment/person/Person.java
@@ -13,5 +13,22 @@ public class Person {
     private LocalDate dateOfBirth;
     private String contactNumber;
     private String email;
-    
+
+/**
+ * Constructs a new Person object with the specified details.
+ *
+ * @param firstName     The first name of the person, not null.
+ * @param lastName      The last name of the person, not null.
+ * @param dateOfBirth   The date of birth of the person , not null.
+ * @param contactNumber The contact number of the person..
+ * @param email         The email address of the person.
+ */
+public Person(String firstName, String lastName, LocalDate dateOfBirth, String contactNumber, String email) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dateOfBirth = dateOfBirth;
+    this.contactNumber = contactNumber;
+    this.email = email;    
+}
+
 }
