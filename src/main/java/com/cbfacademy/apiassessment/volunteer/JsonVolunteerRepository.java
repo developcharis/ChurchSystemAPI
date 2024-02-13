b5d347d26728c5544513dc6ae90f8d2eff8fe10e
Grace Peters <petersograce@gmail.com>
2024-02-13 22:31:13 +0000

    feat: Implement save method in ‘JsonVolunteerRepository’ for volunteer persistence

--- src/main/java/com/cbfacademy/apiassessment/volunteer/JsonVolunteerRepository.java
+++ src/main/java/com/cbfacademy/apiassessment/volunteer/JsonVolunteerRepository.java
@@ -1,13 +1,18 @@
 package com.cbfacademy.apiassessment.volunteer;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import ch.qos.logback.classic.Logger;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration;
 import org.springframework.stereotype.Repository;
 
 
@@ -34,6 +39,27 @@ public class JsonVolunteerRepository implements VolunteerRepository {
         loadVolunteers();
     }
     
+    // Intialisation methods 
     
+    // Loads volunteers from the JSON file into the 'volunteers' list.
+    private void loadVolunteers() {
+        File file = new File(filePath);
+        // Check if the file exists and is not empty
+        if (file.exists() && file.length() != 0) {
+            try (FileReader reader = new FileReader(filePath)) {
+                Type listOfVolunteersType = new TypeToken<ArrayList<Volunteer>>() {}.getType();
+                volunteers = gson.fromJson(reader, listOfVolunteersType);
+                // Handle the case where the JSON file is empty or contains invalid data.
+                if (volunteers == null) volunteers = new ArrayList<>();      
+            } catch (IOException e) {
+                logger.error("Failed to load volunteers from file: {}", filePath, e);
+                volunteers = new ArrayList<>();
+            }    
+        } else {
+            // Initialize an empty list if the file doesn't exist or is empty.
+            volunteers = new ArrayList<>();
+            }  
+        }
+
 
 }
