57db3c49f4b93c1c35a6a707b0690de0d2469ef4
Grace Peters <petersograce@gmail.com>
2024-02-13 16:34:06 +0000

    feat: define a constructor method for the 'Volunteer' class

--- src/main/java/com/cbfacademy/apiassessment/volunteer/Volunteer.java
+++ src/main/java/com/cbfacademy/apiassessment/volunteer/Volunteer.java
@@ -26,4 +26,29 @@ public class Volunteer extends Person {
 
     // Records the date when the volunteer first joined the organization
     private LocalDate dateJoined; 
+
+
+    /**
+     * Constructs a new Volunteer with specified personal and volunteer-specific details.
+     * A unique UUID is generated for each new volunteer to ensure distinct identification..
+     *
+     * @param firstName        The first name of the volunteer.
+     * @param lastName         The last name of the volunteer.
+     * @param dateOfBirth      The date of birth of the volunteer.
+     * @param contactNumber    The contact number of the volunteer.
+     * @param email            The email address of the volunteer.
+     * @param role             The designated role or position of the volunteer within the organization.
+     * @param skills           The skills or expertise the volunteer contributes to their role.
+     * @param isActive         A flag indicating whether the volunteer is currently active.
+     * @param dateJoined       The date on which the volunteer joined the organization, marking the start of their involvement.
+     */
+    public Volunteer(String firstName, String lastName, LocalDate dateOfBirth, String contactNumber, String email, String role, String skills, boolean isActive, LocalDate dateJoined) {
+        super(firstName, lastName, dateOfBirth, contactNumber, email); // Initialize Person attributes
+        this.id = UUID.randomUUID(); // Generate a unique identifier
+        this.role = role;
+        this.skills = skills;
+        this.isActive = isActive;
+        this.dateJoined = dateJoined;
+    }
+
 }
