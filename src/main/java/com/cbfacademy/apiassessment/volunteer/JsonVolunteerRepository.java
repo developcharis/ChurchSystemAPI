3fed5c1587bbd1d0fdf554d63b645296ee6e9020
Grace Peters <petersograce@gmail.com>
2024-02-14 00:34:27 +0000

    feat: Implement core CRUD operations in ‘JsonVolunteerRepository’

--- src/main/java/com/cbfacademy/apiassessment/volunteer/JsonVolunteerRepository.java
+++ src/main/java/com/cbfacademy/apiassessment/volunteer/JsonVolunteerRepository.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -12,7 +13,7 @@ import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
-import org.slf4j.LoggerFactory;
+
 import org.springframework.stereotype.Repository;
 
 import com.google.gson.Gson;
@@ -20,6 +21,10 @@ import com.google.gson.reflect.TypeToken;
 
 import ch.qos.logback.classic.Logger;
 import jakarta.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 
 @Repository
@@ -200,6 +205,71 @@ public class JsonVolunteerRepository implements VolunteerRepository {
     }
 
 
+    // Utility methods
+
+
+    // Helper method to populate the list with initial volunteer data.
+
+    private void populateInitialVolunteers() {
+        // Hardcoded volunteer data for initial setup.
+        volunteers.add(new Volunteer(
+                "Elizabeth", 
+                "John", 
+                LocalDate.of(1980,01,15), 
+                "07584986718", 
+                "lizzi.john@yahoo.com", 
+                "Administrator", 
+                Arrays.asList("Organisational","Attention to detail","Computer skills"), 
+                true, 
+                LocalDate.of(2023,8,15)
+            ));
+            volunteers.add(new Volunteer(
+                "Matthew", 
+                "Lazarus", 
+                LocalDate.of(1962, 12, 07), 
+                "07553698744", 
+                "matthewlazarus12@gmail.com", 
+                "Accountant", 
+                Arrays.asList("Financial analysis", "Attention to detail", "Integrity"), 
+                false, 
+                LocalDate.of(2012, 04, 25)
+            ));
+
+            volunteers.add(new Volunteer(
+                "Rachael", 
+                "Mark", 
+                LocalDate.of(2001, 11, 14), 
+                "07956412843", 
+                "markrachael2001@live.co.uk", 
+                "Greeter", 
+                Arrays.asList("Interpersonal skills", "Warmth", "Sign Language Proficiency"), 
+                true, 
+                LocalDate.of(2024, 01, 18)
+            ));
+
+            volunteers.add(new Volunteer(
+                "Joshua", 
+                "Nun", 
+                LocalDate.of(1990, 01, 01), 
+                "07383555777", 
+                "j.nun@gmail.com", 
+                "Choir Member", 
+                Arrays.asList("Musical talent", "Teamwork", "Performance skills"), 
+                true, 
+                LocalDate.of(2020, 01, 01)
+            ));
+
+            volunteers.add(new Volunteer(
+                "Sarah", 
+                "Bethel", 
+                LocalDate.of(1985, 05, 15), 
+                "07958444888", 
+                "bethelsarah@me.com", 
+                "Accountant", 
+                Arrays.asList("Financial reporting", "Analytical thinking", "Ethics"), 
+                true, 
+                LocalDate.of(2019, 05, 20)
+            ));
+    }
+
 }
-
-
